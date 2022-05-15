f0b62ee1c535d561e916b12602e832bdaf8cd564
cats217 <atsushi.c11@gmail.com>
2022-05-15 19:51:37 +0900

    WebSocketチュートリアル https://qiita.com/omix222/items/a6bd8b896ec48a727cb4
    https://spring.pleiades.io/guides/gs/messaging-stomp-websocket/

---
+++ demo/src/main/java/com/example/demo/websocket/WebSocketGreetingController.java
@@ -0,0 +1,20 @@
+package com.example.demo.websocket;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class WebSocketGreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting(HtmlUtils.htmlEscape(message.getName())
+                + " : "
+                + HtmlUtils.htmlEscape(message.getMessage()) );
+    }
+
+}
